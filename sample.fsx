e04a5a47d4d0f2acc87440b99e359546e6bee602
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2014-11-15 22:22:50 -0800

    static params provided return type

--- sample.fsx
+++ sample.fsx
@@ -1,5 +1,5 @@
 ﻿
-#r @"C:\GitHub\dsyme\SampleMethStaticParamProvider\bin\SampleMethStaticParamProvider.dll"
+#r @"bin\SampleMethStaticParamProvider.dll"
 
 
 open ExampleTypeProvider
@@ -10,4 +10,8 @@ x.ExampleMethWithStaticParam<1>(1) |> printfn "%d"
 x.ExampleMethWithStaticParam<2>(1,2) |> printfn "%d"
 x.ExampleMethWithStaticParam<3>(1,2,3) |> printfn "%d"
 
-
+let dmitry = ExampleType.CreateBag<"Name,Age">(Name = "Dmitry", Age = 40) 
+dmitry.Name <- "Alexei"
+dmitry.Name, dmitry.Age
+let don = ExampleType.CreateBag<"Name,Age,Title">(Name = "Don", Age = 21, Title = "Principal Researcher")
+don.Name, don.Age, don.Title 
